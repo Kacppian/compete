31ab4cee48032c0b73096eab1af1c5eebed8ad64
Kaushik ASP <aspkaushik@gmail.com>
2019-12-15 19:14:08 +0530

    feat: Updates the dom when react updates the data

--- content.js
+++ content.js
@@ -1,7 +1,46 @@
-console.log("Hello from your Chrome extension!");
+let observer;
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "clicked_browser_action") {
-    console.log("LOOOOOOL");
+function generateChallengeDiv() {
+  const button = document.createElement("button");
+  button.innerHTML = "Challenge";
+  button.className = "ui-btn ui-btn-normal primary-cta ui-btn-primary";
+
+  const div = document.createElement("div");
+  div.className = "ctas challenge-others";
+  div.appendChild(button);
+  return div;
+}
+
+function challengeDivExists() {
+  const divs = document.getElementsByClassName("challenge-others");
+  return divs.length > 0;
+}
+
+function injectInviteLinks() {
+  const insertableContainers = document.getElementsByClassName("cta-container");
+  for (let i = 0; i < insertableContainers.length; i += 1) {
+    const node = generateChallengeDiv();
+    insertableContainers[i].appendChild(node);
   }
-});
+}
+
+function callWhenDOMChanged() {
+  if (challengeDivExists()) {
+    console.log("Challenges already exist");
+    return;
+  }
+
+  injectInviteLinks();
+}
+
+function startObservation() {
+  const sections = document.getElementsByClassName("list-container left-pane");
+  if (sections.length < 1) return;
+
+  const watchedSection = sections[0];
+  const config = { attributes: true, childList: true, subtree: true };
+  if (!observer) observer = new MutationObserver(callWhenDOMChanged);
+  observer.observe(watchedSection, config);
+}
+
+startObservation();
