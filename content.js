8155d14095cd38bd097f65a9e8d0074a86c255d2
Kaushik ASP <aspkaushik@gmail.com>
2019-12-15 16:41:50 +0530

    feat: Adds a basic ext setup to print stuff and allow content and background script to communicate

---
+++ content.js
@@ -0,0 +1,7 @@
+console.log("Hello from your Chrome extension!");
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "clicked_browser_action") {
+    console.log("LOOOOOOL");
+  }
+});
